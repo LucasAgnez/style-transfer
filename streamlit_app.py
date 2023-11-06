5ca465d0574c167a913204b2d2126f6612978e6b
Lucas Agnez Lima <56660048+LucasAgnez@users.noreply.github.com>
2023-11-06 00:56:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,6 @@ with col2:
     if style_img:
         st.image(style_img, caption='Style Image')
 
+st.button("Generate!", type="primary")
 st.button("Generate!", type="primary", disabled=!(content_img && style_img), on_click=generate())
 

9f5fd8ed8d099892a738066415a75a22213a5e19
Lucas Agnez Lima <56660048+LucasAgnez@users.noreply.github.com>
2023-11-06 00:10:36 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,13 @@ with st.sidebar:
     st.title("Styele Transfer")
     st.info("This application is originally developed from Tensorflow's Neural Style Transfer Tutorial")
 
-content_img = st.file_uploader("Choose a content image", type=['png', 'jpg', 'jpeg'])
-if content_img:
-    st.image(content_img, caption='Content Image')
-    
-style_img = st.file_uploader("Choose a syle image", type=['png', 'jpg', 'jpeg'])
-if style_img:
-    st.image(style_img, caption='Style Image')
+col1, col2 = st.columns(2)
+
+with col1:
+    content_img = st.file_uploader("Choose a content image", type=['png', 'jpg', 'jpeg'])
+    if content_img:
+        st.image(content_img, caption='Content Image')
+with col2:
+    style_img = st.file_uploader("Choose a syle image", type=['png', 'jpg', 'jpeg'])
+    if style_img:
+        st.image(style_img, caption='Style Image')

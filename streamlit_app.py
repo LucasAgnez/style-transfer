9781c5ac2a8da3ef83e8501db112726eca52612f
lucasagnez <lucas.agnez.064@ufrn.edu.br>
2023-11-28 20:02:01 -0300

    remove imports n usados

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,14 @@
 import streamlit as st
 import tensorflow as tf
 
-
-
-
 def generate(content_img, style_img, epc, itr):
     import os
     import tensorflow as tf
     # Load compressed models from tensorflow_hub
     os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
-    import IPython.display as display
-
-    import matplotlib.pyplot as plt
-    import matplotlib as mpl
-    mpl.rcParams['figure.figsize'] = (12, 12)
-    mpl.rcParams['axes.grid'] = False
-
     import numpy as np 
     import PIL.Image
     import time
-    import functools
 
     imgs = st.empty()
     imgs.image(content_img)
